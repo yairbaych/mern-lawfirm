17b9d469fe36baf6d67b40c41815b943bc45dc36
Yair Bayech <yairbayech99@gmail.com>
2024-02-28 08:30:47 +0200

    added Header component , updated Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes  } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Profile from '../pages/Profile'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
+import Header from './components/Header'
 export default function App() {
   return (
-    <div>
-    
-    </div>
+   <BrowserRouter>
+   <Header />
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
